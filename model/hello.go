8ef8ca699181c853d5a9a5bf0aee8f111f092d62
David Subiros <subiros.david@gmail.com>
2022-02-01 11:07:10 +0000

    remove hello endpoint, model and routes

--- model/hello.go
+++
@@ -1,6 +0,0 @@
-package model
-
-// Hello is an example data model
-type Hello struct {
-	CantabularBlob string `json:"blob"`
-}
--- service/routes.go
+++ service/routes.go
@@ -30,10 +30,9 @@ func (svc *Service) buildRoutes(ctx context.Context) {
 func (svc *Service) publicEndpoints(ctx context.Context) {
 	log.Info(ctx, "enabling public endpoints")
 
-	hello := handler.NewHello(svc.responder, svc.cantabularClient)
+	// Routes
 	areaTypes := handler.NewAreaTypes(svc.responder, svc.cantabularClient)
-	svc.router.Get("/hello", hello.Get)
-	svc.router.Post("/hello", hello.Create)
+
 	svc.router.Get("/area-types", areaTypes.Get)
 }
 
@@ -53,10 +52,8 @@ func (svc *Service) privateEndpoints(ctx context.Context) {
 	r.Use(permissions.Require(auth.Permissions{Read: true}))
 
 	// Routes
-	hello := handler.NewHello(svc.responder, svc.cantabularClient)
 	areaTypes := handler.NewAreaTypes(svc.responder, svc.cantabularClient)
-	r.Get("/hello", hello.Get)
-	r.Post("/hello", permissions.RequireCreate(hello.Create))
+
 	r.Get("/area-types", areaTypes.Get)
 
 	svc.router.Mount("/", r)
