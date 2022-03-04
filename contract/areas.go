9133b559aaddada17d7acb294396f8f9d45c5fd3
fran moore <franmoore05@gmail.com>
2022-03-04 14:13:30 +0000

    Tidying up

--- contract/areas.go
+++ contract/areas.go
@@ -3,7 +3,6 @@ package contract
 import "github.com/ONSdigital/dp-cantabular-dimension-api/model"
 
 // GetAreasRequest defines the schema for the GET /areas query parameter
-
 type GetAreasRequest struct {
 	Dataset  string `schema:"dataset"`
 	AreaType string `schema:"area-type"`
--- model/areas.go
+++ model/areas.go
@@ -1,6 +1,6 @@
 package model
 
-// Areas is an area model with ID and Label
+// Areas is an area model with ID, Label, and area-type
 type Areas struct {
 	ID       string `json:"id"`
 	Label    string `json:"label"`
